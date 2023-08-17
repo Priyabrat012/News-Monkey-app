87bb9b7f8c8c26a906194fcabc6892dc339b252a
prio <priyabrat012@gmail.com>
2023-08-17 14:24:16 +0530

    after resolving the infite scrolling---> showing error coz of the useState that takes time to update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { Component } from 'react'
+import React, { useState }  from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 import {
@@ -10,18 +10,19 @@ import {
 } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
-export default class App extends Component {
-  pageSize = 5;
-  api = process.env.REACT_APP_NEWS_API;
-  state = {
-    progress:0
+const App = () => {
+  const pageSize = 5;
+  const api = process.env.REACT_APP_NEWS_API;
+
+  const [progress, setProgress] = useState(0)
+  
+
+  const setprogress = (progress)=>{
+    setProgress(progress);
+    
   }
 
-  setProgress = (progress)=>{
-    this.setState({progress: progress})
-  }
-
-  render() {
+  
     return (
       <div>
         <Router>
@@ -29,28 +30,28 @@ export default class App extends Component {
         <LoadingBar
         height={3}
         color='#f11946'
-        progress={this.state.progress} 
+        progress={progress} 
       />
       <Routes>
-          <Route exact path="/technology" element={<News setProgress={this.setProgress} api={this.api} key="technology" pageSize={this.pageSize} country="in" category="technology"/>} />
-          <Route exact path="/business" element={<News setProgress={this.setProgress} api={this.api} key="business" pageSize={this.pageSize} country="in" category="business"/>} />
-          <Route exact path="/entertainment" element={<News setProgress={this.setProgress} api={this.api} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/>} />
-          <Route exact path="/general" element={<News setProgress={this.setProgress} api={this.api} key="general" pageSize={this.pageSize} country="in" category="general"/>} />
-          <Route exact path="/health" element={<News setProgress={this.setProgress} api={this.api} key="health" pageSize={this.pageSize} country="in" category="health"/>} />
-          <Route exact path="/science" element={<News setProgress={this.setProgress} api={this.api} key="science" pageSize={this.pageSize} country="in" category="science"/>} />
-          <Route exact path="/sports" element={<News setProgress={this.setProgress} api={this.api} key="sports" pageSize={this.pageSize} country="in" category="sports"/>} />
-          {/* <Route exact path="/technology" element={<News setProgress={this.setProgress} api={this.api} key="technology" pageSize={this.pageSize} country="in" category="technology"/>} /> */}
+          <Route exact path="/technology" element={<News setProgress={  setprogress} api={  api} key="technology" pageSize={  pageSize} country="in" category="technology"/>} />
+          <Route exact path="/business" element={<News setProgress={  setprogress} api={  api} key="business" pageSize={  pageSize} country="in" category="business"/>} />
+          <Route exact path="/entertainment" element={<News setProgress={  setprogress} api={  api} key="entertainment" pageSize={  pageSize} country="in" category="entertainment"/>} />
+          <Route exact path="/general" element={<News setProgress={  setprogress} api={  api} key="general" pageSize={  pageSize} country="in" category="general"/>} />
+          <Route exact path="/health" element={<News setProgress={  setprogress} api={  api} key="health" pageSize={  pageSize} country="in" category="health"/>} />
+          <Route exact path="/science" element={<News setProgress={  setprogress} api={  api} key="science" pageSize={  pageSize} country="in" category="science"/>} />
+          <Route exact path="/sports" element={<News setProgress={  setprogress} api={  api} key="sports" pageSize={  pageSize} country="in" category="sports"/>} />
+          {/* <Route exact path="/technology" element={<News setProgress={  setprogress} api={  api} key="technology" pageSize={  pageSize} country="in" category="technology"/>} /> */}
         
 
         </Routes>
         </Router>
       </div>
     )
-  }
+  
 }
 
 
-
+export default App
 
 
         
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import spinner from './spinner.gif'
-export default class Spinner extends Component {
-  render() {
+const Spinner = () => {
+ 
     return (
       <div className='text-center'>
         <img src={spinner} alt="loading" />
       </div>
     )
-  }
+  
 }
+export default Spinner
